6ff5f36225d37ae95e1f26fb1e71b6666affc117
muse-ema <martin.use@mines-ales.org>
2015-06-06 17:49:14 +0200

    Premières classes métiers
    Ajout des classes TypeColis/Colis/Materiel/Medicament
    Mise à jour diagramme de classe
    Config pour JPA OK !

---
+++ src/escrim/metiers/TypeColis.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package escrim.metiers;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author Martin
+ *
+ */
+@Entity
+public class TypeColis {
+	
+	@Id 
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int uid;
+	private float hauteur;
+	private float longueur;
+	private float largeur;
+	private float volume;
+	private float poidsMax;
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public float getHauteur() {
+		return hauteur;
+	}
+	public void setHauteur(float hauteur) {
+		this.hauteur = hauteur;
+	}
+	public float getLongueur() {
+		return longueur;
+	}
+	public void setLongueur(float longueur) {
+		this.longueur = longueur;
+	}
+	public float getLargeur() {
+		return largeur;
+	}
+	public void setLargeur(float largeur) {
+		this.largeur = largeur;
+	}
+	public float getVolume() {
+		return volume;
+	}
+	public void setVolume(float volume) {
+		this.volume = volume;
+	}
+	public float getPoidsMax() {
+		return poidsMax;
+	}
+	public void setPoidsMax(float poidsMax) {
+		this.poidsMax = poidsMax;
+	}
+	
+
+}
