a97ce234dc46a7f67d60fdcf7e690b1db62ba0b3
muse-ema <martin.use@mines-ales.org>
2015-06-06 17:49:14 +0200

    Premières classes métiers
    Ajout des classes TypeColis/Colis/Materiel/Medicament
    Mise à jour diagramme de classe
    Config pour JPA OK !

---
+++ src/escrim/metiers/Medicaments.java
@@ -0,0 +1,85 @@
+/**
+ * 
+ */
+package escrim.metiers;
+
+import javax.persistence.Entity;
+
+/**
+ * @author Martin
+ *
+ */
+@Entity
+public class Medicaments extends Materiel {
+	
+	private String principeActif;
+	private String dosage;
+	private String dlu;
+	private String lot;
+	//Classe thérapeuthique
+	private String classeThera;
+	private int dotationU7;
+	
+
+	public String getPrincipeActif() {
+		return principeActif;
+	}
+
+	public void setPrincipeActif(String principeActif) {
+		this.principeActif = principeActif;
+	}
+
+	public String getDosage() {
+		return dosage;
+	}
+
+	public void setDosage(String dosage) {
+		this.dosage = dosage;
+	}
+
+	public String getDlu() {
+		return dlu;
+	}
+
+	public void setDlu(String dlu) {
+		this.dlu = dlu;
+	}
+
+	public String getLot() {
+		return lot;
+	}
+
+	public void setLot(String lot) {
+		this.lot = lot;
+	}
+
+	public String getClasseThera() {
+		return classeThera;
+	}
+
+	public void setClasseThera(String classeThera) {
+		this.classeThera = classeThera;
+	}
+
+	public int getDotationU7() {
+		return dotationU7;
+	}
+
+	public void setDotationU7(int dotationU7) {
+		this.dotationU7 = dotationU7;
+	}
+
+	public Medicaments() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Medicaments(int id, String denomation, Colis colis,
+			String observations, int quantite) {
+		super(id, denomation, colis, observations, quantite);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
