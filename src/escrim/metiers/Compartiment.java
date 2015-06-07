26c9df2b0e6709f1f1dd25aadd1ac117f096e08b
muse-ema <martin.use@mines-ales.org>
2015-06-07 15:25:04 +0200

    Structure des classes métiers terminée, à revoir une fois que les premiers tests auront lieu pour vérifier la cohérence des données.

---
+++ src/escrim/metiers/Compartiment.java
@@ -0,0 +1,68 @@
+package escrim.metiers;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+
+@Entity
+public class Compartiment {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int uid;
+	private float hauteur;
+	private float largeur;
+	private float longueur;
+	private float volume;
+	private float poids;
+	@OneToMany
+	private List<Colis> ColisDansCompartiement;
+	
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public float getHauteur() {
+		return hauteur;
+	}
+	public void setHauteur(float hauteur) {
+		this.hauteur = hauteur;
+	}
+	public float getLargeur() {
+		return largeur;
+	}
+	public void setLargeur(float largeur) {
+		this.largeur = largeur;
+	}
+	public float getLongueur() {
+		return longueur;
+	}
+	public void setLongueur(float longueur) {
+		this.longueur = longueur;
+	}
+	public float getVolume() {
+		return volume;
+	}
+	public void setVolume(float volume) {
+		this.volume = volume;
+	}
+	public float getPoids() {
+		return poids;
+	}
+	public void setPoids(float poids) {
+		this.poids = poids;
+	}
+	public List<Colis> getColisDansCompartiement() {
+		return ColisDansCompartiement;
+	}
+	public void setColisDansCompartiement(List<Colis> colisDansCompartiement) {
+		ColisDansCompartiement = colisDansCompartiement;
+	}
+}
---
+++ src/escrim/metiers/Transport.java
@@ -0,0 +1,48 @@
+package escrim.metiers;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+
+@Entity
+public class Transport {
+	
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int uid;
+	private String denomination;
+	private float poidsMax;
+	@OneToMany
+	private List<Compartiment> listeCompartiments;
+	
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getDenomination() {
+		return denomination;
+	}
+	public void setDenomination(String denomination) {
+		this.denomination = denomination;
+	}
+	public float getPoidsMax() {
+		return poidsMax;
+	}
+	public void setPoidsMax(float poidsMax) {
+		this.poidsMax = poidsMax;
+	}
+	public List<Compartiment> getListeCompartiments() {
+		return listeCompartiments;
+	}
+	public void setListeCompartiments(List<Compartiment> listeCompartiments) {
+		this.listeCompartiments = listeCompartiments;
+	}
+}
