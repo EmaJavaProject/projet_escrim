73644fca7ea2f9998a853aa883c5479ab24f6bcc
Maxime <maxime.genaudet@gmail.com>
2015-06-07 03:30:01 +0200

    nouvelles icones + premier jet sur les onglets principaux + popup de confirmation qui va bien

--- src/ihm/Main_gui.java
+++ src/ihm/Main_gui.java
@@ -2,34 +2,20 @@ package ihm;
 
 import java.awt.EventQueue;
 
-import javax.swing.BorderFactory;
-import javax.swing.Icon;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
-import javax.swing.JPanel;
+import javax.swing.JOptionPane;
 import javax.swing.JTabbedPane;
-
-import javax.swing.JPopupMenu;
-
 import java.awt.Component;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
-
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class Main_gui {
-	private boolean clicked = true;
-	private JFrame frame;
-	/**
-	 * @wbp.nonvisual location=69,-36
-	 */
-	private final JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+	// private boolean clicked = true;
+	private JFrame frmEscrim;
 
 	/**
 	 * Launch the application.
@@ -39,7 +25,7 @@ public class Main_gui {
 			public void run() {
 				try {
 					Main_gui window = new Main_gui();
-					window.frame.setVisible(true);
+					window.frmEscrim.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -58,71 +44,122 @@ public class Main_gui {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setResizable(false);
-		frame.setBounds(100, 100, 1024, 768);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JMenuBar menuBar = new JMenuBar();
-		frame.setJMenuBar(menuBar);
-		
-		JMenu mnFichier = new JMenu("Fichier");
-		menuBar.add(mnFichier);
-		
-		JMenuItem mntmImporter = new JMenuItem("Importer...");
-		mnFichier.add(mntmImporter);
-		
-		JMenuItem mntmExporter = new JMenuItem("Exporter...");
-		mnFichier.add(mntmExporter);
-		
-		JMenu mnQuitter = new JMenu("Quitter");
-		menuBar.add(mnQuitter);
-		frame.getContentPane().setLayout(null);
-		
-		JPanel panel = new JPanel();
-		panel.setBounds(12, 13, 151, 615);
-		frame.getContentPane().add(panel);
-		
-		
-		Icon buttonIcon = new ImageIcon("images/ICONE_AVION.jpg");
-		Icon buttonIcon2 = new ImageIcon("images/ICONE_AVION_GRIS.jpg");
-		JButton button = new JButton(buttonIcon);
-		button.setToolTipText("Avion");
-		button.setBorder(BorderFactory.createEmptyBorder());
-		ActionListener action = new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (clicked) {
-					button.setIcon(buttonIcon2);
-					clicked = false;
-				} else {
-					button.setIcon(buttonIcon);
-					clicked = true;
+		frmEscrim = new JFrame();
+		frmEscrim.setTitle("ESCRIM");
+		frmEscrim.setResizable(false);
+		frmEscrim.setBounds(100, 100, 1024, 768);
+		frmEscrim.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		frmEscrim.addWindowListener(new java.awt.event.WindowAdapter() {
+			@Override
+			public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+				String ObjButtons[] = { "Oui", "Non" };
+				int PromptResult = JOptionPane.showOptionDialog(null,
+						"Etes vous sûr de vouloir quitter ?",
+						"Quitter l'application", JOptionPane.DEFAULT_OPTION,
+						JOptionPane.WARNING_MESSAGE, null, ObjButtons,
+						ObjButtons[1]);
+				if (PromptResult == JOptionPane.YES_OPTION) {
+					System.exit(0);
 				}
-
-			}
-		};	
-		button.addActionListener(action);
-
-			button.setContentAreaFilled(false);
-			panel.add(button);
-		
-		
-	}
-	private static void addPopup(Component component, final JPopupMenu popup) {
-		component.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			public void mouseReleased(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			private void showMenu(MouseEvent e) {
-				popup.show(e.getComponent(), e.getX(), e.getY());
 			}
 		});
+
+		JMenuBar menu = new JMenuBar();
+		frmEscrim.setJMenuBar(menu);
+
+		JMenu mnFichier = new JMenu("Fichier");
+		menu.add(mnFichier);
+
+		JMenuItem mntmImporter = new JMenuItem("Importer...");
+		mnFichier.add(mntmImporter);
+
+		JMenuItem mntmExporter = new JMenuItem("Exporter...");
+		mnFichier.add(mntmExporter);
+
+		JMenuItem mnQuitter = new JMenuItem("Quitter");
+		mnQuitter.addActionListener(new QuitterAppli());
+		menu.add(mnQuitter);
+
+		JTabbedPane panelPrincipal = new JTabbedPane(JTabbedPane.LEFT);
+		panelPrincipal.setName("");
+		panelPrincipal.setBounds(0, 0, 1018, 707);
+		frmEscrim.getContentPane().add(panelPrincipal);
+
+		JTabbedPane stock = new JTabbedPane(JTabbedPane.TOP);
+		stock.setAlignmentY(Component.TOP_ALIGNMENT);
+		stock.setAlignmentX(Component.LEFT_ALIGNMENT);
+		panelPrincipal
+				.addTab("Stock",
+						new ImageIcon(
+								"images/icone_stock.jpg"),
+						stock, null);
+
+		JTabbedPane conteneurStock = new JTabbedPane(JTabbedPane.TOP);
+		stock.addTab("Conteneur", null, conteneurStock, null);
+
+		JTabbedPane contenuStock = new JTabbedPane(JTabbedPane.TOP);
+		stock.addTab("Contenu", null, contenuStock, null);
+
+		JTabbedPane avion = new JTabbedPane(JTabbedPane.LEFT);
+		avion.setAlignmentY(Component.TOP_ALIGNMENT);
+		avion.setAlignmentX(Component.LEFT_ALIGNMENT);
+		panelPrincipal
+				.addTab("Avion",
+						new ImageIcon(
+								"images/icone_avion.jpg"),
+						avion, null);
+		panelPrincipal.setEnabledAt(1, true);
+
+		JTabbedPane localisation = new JTabbedPane(JTabbedPane.TOP);
+		localisation.setAlignmentY(Component.TOP_ALIGNMENT);
+		localisation.setAlignmentX(Component.LEFT_ALIGNMENT);
+		panelPrincipal.addTab("Localisation", new ImageIcon(
+				"images/icone_localiser.jpg"),
+				localisation, null);
+
+		JTabbedPane conteneurLocalisation = new JTabbedPane(JTabbedPane.TOP);
+		conteneurLocalisation.setAlignmentY(Component.TOP_ALIGNMENT);
+		localisation.addTab("Conteneur", null, conteneurLocalisation, null);
+
+		JTabbedPane contenuLocalisation = new JTabbedPane(JTabbedPane.TOP);
+		localisation.addTab("Contenu", null, contenuLocalisation, null);
+
+		// Icon buttonIcon = new ImageIcon("images/ICONE_AVION.jpg");
+		// Icon buttonIcon2 = new ImageIcon("images/ICONE_AVION_GRIS.jpg");
+		// JButton button = new JButton(buttonIcon);
+		// button.setToolTipText("Avion");
+		// button.setBorder(BorderFactory.createEmptyBorder());
+		// ActionListener action = new ActionListener() {
+		// public void actionPerformed(ActionEvent e) {
+		// if (clicked) {
+		// button.setIcon(buttonIcon2);
+		// clicked = false;
+		// } else {
+		// button.setIcon(buttonIcon);
+		// clicked = true;
+		// }
+		//
+		// }
+		// };
+		// button.addActionListener(action);
+
+		// button.setContentAreaFilled(false);
+		// panel.add(button);
+
 	}
+
+	static class QuitterAppli implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			String ObjButtons[] = { "Oui", "Non" };
+			int PromptResult = JOptionPane.showOptionDialog(null,
+					"Etes vous sûr de vouloir quitter ?",
+					"Quitter l'application", JOptionPane.DEFAULT_OPTION,
+					JOptionPane.WARNING_MESSAGE, null, ObjButtons,
+					ObjButtons[1]);
+			if (PromptResult == JOptionPane.YES_OPTION) {
+				System.exit(0);
+			}
+		}
+	}
+
 }
