de17d1080cec1f6a284af567432cfb476048d792
muse-ema <martin.use@mines-ales.org>
2015-06-18 23:51:34 +0200

    Dao pour compartiment avec un update qui marche peu importe le nombre de champs a update !

--- src/escrim/manager/CompartimentManager.java
+++ src/escrim/manager/CompartimentManager.java
@@ -1,21 +1,49 @@
 package escrim.manager;
 
+import java.util.List;
+
+import escrim.dao.CompartimentDao;
+import escrim.metiers.Colis;
+import escrim.metiers.Compartiment;
+import escrim.utils.EscrimDatabase;
 
 public class CompartimentManager {
 
-	public void createCompartiment() {
+	public static void createCompartiment(float hauteur, float largeur,
+			float longueur, float volume, float poids,
+			List<Colis> colisDansCompartiment) {
+		Compartiment compartiment = new Compartiment();
+		compartiment.setHauteur(hauteur);
+		compartiment.setLargeur(largeur);
+		compartiment.setLongueur(longueur);
+		compartiment.setListeColisDansCompartiment(colisDansCompartiment);
+		compartiment.setVolume(volume);
+		CompartimentDao.create(compartiment);
 
 	}
 
-	public void updateCompartiment() {
+	public static void updateCompartiment(int uid, float hauteur,
+			float largeur, float longueur, float volume, float poids,
+			List<Colis> colisDansCompartiment) {
+
+		Compartiment tempon = loadCompartiment(uid);
+		tempon.setHauteur(hauteur);
+		tempon.setLargeur(largeur);
+		tempon.setLongueur(longueur);
+		tempon.setListeColisDansCompartiment(colisDansCompartiment);
+		tempon.setVolume(volume);
+		CompartimentDao.update(tempon, uid);
 
 	}
 
-	public void deleteCompartiment() {
+	public void deleteCompartiment(int uid) {
 
 	}
 
-	public void loadCompartiment() {
+	public static Compartiment loadCompartiment(int uid) {
+		Compartiment compartiment = EscrimDatabase.getInstance().getEm()
+				.find(Compartiment.class, uid);
+		return compartiment;
 
 	}
 
