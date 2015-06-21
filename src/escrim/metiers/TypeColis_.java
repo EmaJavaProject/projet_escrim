6d66a8552fd331eea13ef3928924ef1d7c927dae
Thrayn38 <valentin.vincent2@gmail.com>
2015-06-21 22:36:48 +0200

    dzdadzadadzada

---
+++ src/escrim/metiers/TypeColis_.java
@@ -0,0 +1,17 @@
+package escrim.metiers;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2015-06-21T21:06:41.441+0200")
+@StaticMetamodel(TypeColis.class)
+public class TypeColis_ {
+	public static volatile SingularAttribute<TypeColis, Integer> uid;
+	public static volatile SingularAttribute<TypeColis, String> designation;
+	public static volatile SingularAttribute<TypeColis, Float> hauteur;
+	public static volatile SingularAttribute<TypeColis, Float> longueur;
+	public static volatile SingularAttribute<TypeColis, Float> largeur;
+	public static volatile SingularAttribute<TypeColis, Float> volume;
+	public static volatile SingularAttribute<TypeColis, Float> poidsMax;
+}
