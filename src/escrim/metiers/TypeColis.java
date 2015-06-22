428d1b4c899e0325896a0c0056f1ecf7710206ea
Whitz0 <leclercq.thomas07@gmail.com>
2015-06-22 21:31:01 +0200

    Ajout de TypeColis.java

---
+++ src/escrim/metiers/TypeColis.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package escrim.metiers;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author Martin
+ *
+ */
+@Entity
+public class TypeColis implements Metier {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int uid;
+	private String designation;
+	private float hauteur;
+	private float longueur;
+	private float largeur;
+	private float volume;
+	private float poidsMax;
+
+	public int getUid() {
+		return uid;
+	}
+
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+
+	public float getHauteur() {
+		return hauteur;
+	}
+
+	public void setHauteur(float hauteur) {
+		this.hauteur = hauteur;
+	}
+
+	public float getLongueur() {
+		return longueur;
+	}
+
+	public void setLongueur(float longueur) {
+		this.longueur = longueur;
+	}
+
+	public float getLargeur() {
+		return largeur;
+	}
+
+	public void setLargeur(float largeur) {
+		this.largeur = largeur;
+	}
+
+	public float getVolume() {
+		return volume;
+	}
+
+	public void setVolume(float volume) {
+		this.volume = volume;
+	}
+
+	public float getPoidsMax() {
+		return poidsMax;
+	}
+
+	public void setPoidsMax(float poidsMax) {
+		this.poidsMax = poidsMax;
+	}
+
+	public String getDesignation() {
+		return designation;
+	}
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+}
