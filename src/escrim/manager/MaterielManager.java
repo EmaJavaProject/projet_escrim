f15dea09c893adc7cabdaf1835914d2128581447
mathieu975 <mathieu.urtiz@gmail.com>
2015-06-23 22:41:59 +0200

    merge

---
+++ src/escrim/manager/MaterielManager.java
@@ -0,0 +1,38 @@
+package escrim.manager;
+
+import java.util.List;
+
+import escrim.dao.MaterielDao;
+import escrim.metiers.Materiel;
+
+public class MaterielManager {
+
+	public void createMateriel(String denomination, String observation, int quantite) {
+		Materiel materiel = new Materiel();
+		materiel.setDenomination(denomination);
+		materiel.setObservations(observation);
+		materiel.setQuantite(quantite);
+		MaterielDao.create(materiel);
+	}
+
+	public void updateMateriel(int uid, String denomination, String observation, int quantite) {
+		Materiel materielTemporaire = loadMateriel(uid);
+		materielTemporaire.setDenomination(denomination);
+		materielTemporaire.setObservations(observation);
+		materielTemporaire.setQuantite(quantite);
+		MaterielDao.update(materielTemporaire, uid);
+	}
+
+	public void deleteMateriel(int uid) {
+		Materiel deleteMateriel = loadMateriel(uid);
+		MaterielDao.remove(deleteMateriel);
+	}
+
+	public static Materiel loadMateriel(int uid) {
+		return MaterielDao.load(uid);
+	}
+	public static List<Materiel> loadAllMateriels(){
+		return MaterielDao.loadAll();
+	}
+
+}
